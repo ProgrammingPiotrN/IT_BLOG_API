d80706daac03732c744406b4c3c07627d53e095c
Piotr Nawrocki <piotr_naw99@outlook.com>
2024-09-22 09:20:24 +0200

    Instalacja laravel passport, wprowadzenie zmian w modelu user

--- app/Domain/Models/User.php
+++ app/Domain/Models/User.php
@@ -6,21 +6,45 @@ namespace App\Domain\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public string $name;
+    public string $email;
+    public string $password;
+
+    private function __construct() {}
+
+    public static function createFromString(string $user):self
+    {
+        $data = json_decode($user, true);
+        return self::createFromArray($data);
+    }
+
+    public static function createFromArray(array $data):self
+    {
+        $user = new self();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+        return $user;
+
+    }
+
+    public static function createEmpty(): self
+    {
+        return new self();
+    }
+
+
 
     /**
      * The attributes that should be hidden for serialization.
