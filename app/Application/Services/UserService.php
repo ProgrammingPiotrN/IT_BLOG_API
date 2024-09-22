e54acc560095b606d30b5e9c0915445e1cabefd4
Piotr Nawrocki <piotr_naw99@outlook.com>
2024-09-22 10:20:38 +0200

    Modyfikacja UserService

--- app/Application/Services/UserService.php
+++ app/Application/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Application\Services;
 
 use App\Application\DTOs\UserDTO;
+use App\Domain\Exceptions\UserCreationException;
 use App\Domain\Interfaces\UserRepositoryInterface;
 use App\Domain\Models\User;
 
@@ -20,12 +21,24 @@ class UserService
 
     public function createUser(UserDTO $userDTO): User
     {
+        // Walidacja dla pustego imienia
+        if (empty($userDTO->name)) {
+            throw UserCreationException::withEmptyName();
+        }
+
+        // Walidacja dla zbyt krótkiego imienia
+        if (strlen($userDTO->name) < 3) {
+            throw UserCreationException::withNameTooShort(3);
+        }
+
+        // Utworzenie użytkownika
         $user = User::createFromArray([
             'name' => $userDTO->name,
             'email' => $userDTO->email,
-            'password' => $userDTO->password,
+            'password' => bcrypt($userDTO->password),
         ]);
 
+        // Zapis użytkownika w repozytorium
         return $this->repository->save($user);
     }
 }
